260ca5934b583e10149fb036046a89f5fac39609
DavTV <atellovalqui@gmail.com>
2023-03-13 22:40:31 -0500

    Actualización vista news col

--- src/pages/components/CardNew.js
+++ src/pages/components/CardNew.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image"
 const CardNew = ({title,previo,ruta,image}) => {
     return ( 
-        <div class="col-6 col-md-4 p-2 ">
+        <div class="col-12 col-md-4 p-2 ">
         <div class=" card card-body">
             <div className="containerCarNew">
                 <Image width={0} height={0} layout='responsive'  src={image} alt={title}/>
