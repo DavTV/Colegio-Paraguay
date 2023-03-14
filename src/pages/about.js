b3c8ef795df502a08b523ac3020e9d855ba11ffb
DavTV <atellovalqui@gmail.com>
2023-03-13 21:44:27 -0500

    Actualización imagenes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Slider from "./components/Slider";
 const About = () => {
     return ( 
         <div>
@@ -24,58 +25,13 @@ const About = () => {
                     </p> */}
                 </div>
                 <div className="col-12 col-md-5  d-flex justify-content-center">
-                    <Image  src="/imagen1.jpg" width={300} height={400} className="imagen-about " />
+                    <Image  src="/img-about.jpg" width={300} height={400} className="imagen-about " />
                 </div>
             </div>
             <hr/>
 
         </div>
-        <div className="container row mx-auto align-items-center">
-        <div id="carouselExampleIndicators" class="carousel slide col-12 col-md-6">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner slider-sizes my-5">
-            <div class="carousel-item active">
-            <img src="/slider1.webp" class="d-block w-100" alt="..."/>
-            </div>
-            <div class="carousel-item">
-            <img src="slider2.png" class="d-block w-100" alt="..."/>
-            </div>
-            <div class="carousel-item">
-            <img src="/slider3.jpg" class="d-block w-100" alt="..."/>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-        </div>
-        <div className="col-12 col-md-6">
-        <p>
-        La IE Colegio 1146 Republica Del Paraguay busca progresar personas facultadas con una alta autoestima, conocimientos, capacidades sociales y una estable enseñanza académica, moral y emocional para que puedan obtener el éxito personal y profesional dentro de una sociedad moderna y cambiante.
-
-
-
-        </p>
-        <p>
-        En institucioneducativa sabemos que en Lima esta I.E. facilita una educación de alta calidad en un sector de ejercitamiento seguro, en donde nuestros alumnos logran su total crecimiento espiritual, intelectual, moral, físico, social y emocional.
-
-        </p>
-        {/* <p>
-        La IE Colegio 1146 Republica Del Paraguay busca progresar personas facultadas con una alta autoestima, conocimientos, capacidades sociales y una estable enseñanza académica, moral y emocional para que puedan obtener el éxito personal y profesional dentro de una sociedad moderna y cambiante.
-
-        </p> */}
-
-
-        </div>
-        </div>
+        <Slider />
             <div className=" bg-danger text-white">
                 <div className="row  container mx-auto">
 
---
+++ src/pages/contact.js
@@ -0,0 +1,37 @@
+
+const Contact = () => {
+    const handleSubmit=(e) => {
+        e.preventDefault();
+        alert("Formulario Enviado con éxito!");
+    }
+    return ( 
+        <form className="my-4 w-70-50 mx-auto " onSubmit={handleSubmit}>      
+        <h3 className="my-5 text-center">Envianos un correo</h3>
+        <hr/>
+                <div className="mb-3 text-start ">
+                    <label className="form-label">Nombre Completo <span className="text-danger">*</span></label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="mb-3 text-start ">
+                    <label className="form-label">Asunto <span className="text-danger">*</span></label>
+                    <input type="text" className="form-control" />
+                </div>
+            
+                <div className="mb-3 text-start ">
+                    <label className="form-label">Email <span className="text-danger">*</span></label>
+                    <input type="e-mail" className="form-control" />
+                </div>
+                
+                <div className="mb-3 text-start ">
+                    <label className="form-label">Comentario </label>
+                    <textarea className="form-control"></textarea>
+                </div>
+                <div className="mb-3 text-start ">
+                    <button className="btn btn-danger">Enviar</button>
+                </div>
+            </form>
+     );
+}
+ 
+export default Contact;
