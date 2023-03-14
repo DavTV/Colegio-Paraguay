e9a62bd2fcf8b90608e42c5b83058b18f1e13537
DavTV <atellovalqui@gmail.com>
2023-03-13 20:56:14 -0500

    Actualización index info, agregado de slider en about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,26 +5,76 @@ const About = () => {
         <div className='py-3 bg-danger'></div>
 
         <div className="container text-center">
-            <h2 className="my-5">BIENVENIDOS</h2>
+            <h2 className="my-5">Conoce más sobre nosotros</h2>
             <hr/>
-            <div className="row my-5 px-3">
+            <div className="row my-5 px-3 text-start ">
                 <div className="col-12 col-md-6 my-4">
-                    <h3>!Bienvenidos a nuestro colegio¡</h3>
+                    <h3 className="text-center">!Bienvenidos a nuestro colegio¡</h3>
                     <p>
-                    El Fleming College es una comunidad cálida y amigable, que alienta a los estudiantes a convertirse en ciudadanos del mundo altamente exitosos y con principios.
+                    La Institución educativa Colegio 1146 Republica Del Paraguay se sitúa en el lugar de Lima Cercado, provincia de Lima, ésta institución depende de la UGEL 03 BREÑA que revisa la institución educativa, y esta última pertenece a la Dirección regional de educación DRE LIMA METROPOLITANA.
+
+
+
                     </p>
                     <p>
-                    Ofrecemos un currículo amplio y equilibrado, basado en los mejores estándares británicos, peruanos e internacionales, y estamos orgullosos de ser el primer Colegio del Mundo del IB en Trujillo. A través de nuestro plan de estudios dinámico basado en conceptos y nuestro sistema pastoral, alentamos a nuestros alumnos a desarrollar las cualidades de la pasión, la solidaridad, la perseverancia y la mentalidad internacional.
+                    La Institución educativa Colegio 1146 Republica Del Paraguay, es más que una Institución Educativa, es un contexto de familia, un equipo de profesionales altamente competitivos que vigilan para que los alumnos se integren adecuadamente, tanto en su vida personal como también social y escolar.
                     </p>
-                    <p>
+                    {/* <p>
                     Nuestros profesores guían a nuestros alumnos para que se conviertan en aprendices independientes, a través de un proceso de indagación y otras actividades estimulantes, lo que les permite alcanzar sus objetivos de aprendizaje.
-                    </p>
+                    </p> */}
                 </div>
-                <div className="col-12 col-md-6">
-                    <Image  src="/imagen1.jpg" width={300} height={400} className="imagen-about" />
+                <div className="col-12 col-md-5  d-flex justify-content-center">
+                    <Image  src="/imagen1.jpg" width={300} height={400} className="imagen-about " />
                 </div>
             </div>
+            <hr/>
 
+        </div>
+        <div className="container row mx-auto align-items-center">
+        <div id="carouselExampleIndicators" class="carousel slide col-12 col-md-6">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner slider-sizes my-5">
+            <div class="carousel-item active">
+            <img src="/slider1.webp" class="d-block w-100" alt="..."/>
+            </div>
+            <div class="carousel-item">
+            <img src="slider2.png" class="d-block w-100" alt="..."/>
+            </div>
+            <div class="carousel-item">
+            <img src="/slider3.jpg" class="d-block w-100" alt="..."/>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+        <div className="col-12 col-md-6">
+        <p>
+        La IE Colegio 1146 Republica Del Paraguay busca progresar personas facultadas con una alta autoestima, conocimientos, capacidades sociales y una estable enseñanza académica, moral y emocional para que puedan obtener el éxito personal y profesional dentro de una sociedad moderna y cambiante.
+
+
+
+        </p>
+        <p>
+        En institucioneducativa sabemos que en Lima esta I.E. facilita una educación de alta calidad en un sector de ejercitamiento seguro, en donde nuestros alumnos logran su total crecimiento espiritual, intelectual, moral, físico, social y emocional.
+
+        </p>
+        {/* <p>
+        La IE Colegio 1146 Republica Del Paraguay busca progresar personas facultadas con una alta autoestima, conocimientos, capacidades sociales y una estable enseñanza académica, moral y emocional para que puedan obtener el éxito personal y profesional dentro de una sociedad moderna y cambiante.
+
+        </p> */}
+
+
+        </div>
         </div>
             <div className=" bg-danger text-white">
                 <div className="row  container mx-auto">
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="container">
             <nav class="navbar navbar-expand-lg navbar-light ">
                 <div class="container-fluid">
-                    <Link href="/" > <Image className="social" src="/colegioLogo.jpeg"  width={50} height={50} /> </Link>
+                    <Link href="/" > <Image className="social" src="/colegioLogo.png"  width={75} height={50} /> </Link>
                     {/* <a class="navbar-brand" href="#"></a> */}
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -18,24 +18,24 @@ const Header = () => {
                                 <Link className="nav-link" href="/about">Nosotros</Link>
                             </li>
                             <li class="nav-item">
-                                <Link className="nav-link" href="/matricula">Matrículas 2023</Link>
-                            </li>
-                            <li class="nav-item">
                                 <Link className="nav-link" href="/news">Noticias</Link>
                             </li>
                             <li class="nav-item">
-                                <Link className="nav-link" href="/services">Servicios</Link>
+                                <Link className="nav-link" href="/matricula">Matrículas 2023</Link>
                             </li>
                             <li class="nav-item">
-                                <Link className="nav-link" href="/contact">Contáctanos</Link>
+                                <Link className="nav-link" href="/services">Servicios</Link>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Talleres</a>
                                 <div class="dropdown-menu bg-secondary">
                                     <Link className="dropdown-item" href="/taller/dance">Danzas</Link>
                                     <Link className="dropdown-item" href="/taller/futbol">Futbol</Link>
-                                    <Link className="dropdown-item" href="/taller/swimming">Natación</Link>
+                                    <Link className="dropdown-item" href="/taller/paint">Taller de pintura</Link>
                                 </div>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" href="/contact">Contáctanos</Link>
                             </li>
                         </ul>
                     </div>
