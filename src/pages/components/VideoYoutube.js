d475043f12b7c2a907412177af907b778ab352ff
DavTV <atellovalqui@gmail.com>
2023-03-16 15:01:53 -0500

    Actualización entrega final - Loading.

--- src/pages/components/VideoYoutube.js
+++ src/pages/components/VideoYoutube.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { useEffect } from "react";
-
+import Image from "next/image"
 const VideoYoutube = () => {
-  const [loading, setLoading] = useState(false);
+  const [none, setNone] = useState("none");
+  const [block, setBlock] = useState("block");
     const timeLoading=()=>{
           setTimeout(() => {
-            setLoading(true);
-          }, 500);
+            setNone("block");
+            setBlock("none");
+          }, 700);
     }
     useEffect(()=>{
       timeLoading()
@@ -14,10 +16,15 @@ const VideoYoutube = () => {
     return (  
         <div className="my-5">
         <h3>Matriculate tú mismo</h3>
-        {
-          loading == false? <p className="my-5 py-5">Cargando ...</p>:
-        <iframe width="90%" height="400" src="https://www.youtube.com/embed/bypztHMvrw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        }
+        
+          <div className="my-5 p-5" style={{ 
+                                display:`${block}` 
+                                }}><Image  src="/loading.svg" alt="loading" width={100} height={100}  /> <small>Cargando video</small></div>
+          <iframe style={{ 
+                                display:`${none}` 
+                                }}
+                                 width="90%" height="400" src="https://www.youtube.com/embed/bypztHMvrw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen  ></iframe>
+        
       </div>
     );
 }
